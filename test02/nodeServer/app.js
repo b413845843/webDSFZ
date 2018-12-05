aa1cfbfe268efd9213dd209916f5260226967d0a
chenguobin <chengb@dascom.cn>
2018-12-05 15:23:46 +0800

    /wx 增加获取acesstoken deviceid 接口

--- test02/nodeServer/app.js
+++ test02/nodeServer/app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var history = require('connect-history-api-fallback');
+var bodyParser = require('body-parser');
+
 // var proxy = require('http-proxy-middleware')
 
 // 路由
@@ -19,17 +21,15 @@ var app = express();
 
 // 配合vue的history模式
 app.use(history({
-  rewrites: [
-    {
+  rewrites: [{
       from: /^\/wx\/.*$/,
-      to: function(context) {
-        console.log('/wx' + context.parsedUrl.pathname);
+      to: function (context) {
         return context.parsedUrl.pathname;
       }
     },
     {
       from: /^\/users\/.*$/,
-      to: function(context) {
+      to: function (context) {
         return context.parsedUrl.pathname;
       }
     }
@@ -47,10 +47,15 @@ app.set('jwtsecret', config.jwtsecret)
 // 默认
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 // app.use(jwt({ secret: Buffer.from(config.jwtsecret, 'base64') }).unless({ path: ['/login'] }));
 
@@ -60,7 +65,7 @@ app.use('/api', apiRouter);
 app.use('/wx', wxRouter)
 
 //  设置允许跨域访问该服务.
-app.all('*', function(req, res, next) {
+app.all('*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   //  Access-Control-Allow-Headers ,可根据浏览器的F12查看,把对应的粘贴在这里就行
   res.header('Access-Control-Allow-Headers', 'Content-Type');
@@ -70,12 +75,12 @@ app.all('*', function(req, res, next) {
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- test02/nodeServer/routes/wx.js
+++ test02/nodeServer/routes/wx.js
@@ -1,16 +1,119 @@
 let express = require('express')
+let fs = require('fs')
+var readline = require('readline');
+let path = require('path')
+let sha1 = require('sha1')
 let router = express.Router()
+let axios = require('axios')
 
 const loggerFactory = require('../log/log4js')
-
 const wxLogger = loggerFactory.getLogger('wx')
+const logPath = path.join(__dirname, '../log/logs/wx_file.log')
 
+const token = 'dascom666'
+const productID = 51504
+const deviceIDUrl = 'https://api.weixin.qq.com/device/getqrcode'
+// 按行读取log文件
+function readFileToArr(fReadName, callback) {
+  var fRead = fs.createReadStream(fReadName);
+  var objReadline = readline.createInterface({
+    input: fRead
+  });
+  var arr = [];
+  objReadline.on('line', function (line) {
+    arr.push(line);
+    // console.log('line:'+ line);
+  });
+  objReadline.on('close', function () {
+    // console.log(arr);
+    callback(arr);
+  });
+}
+
+// 微信平台校验
 router.get('/', (req, res, next) => {
-  console.log('cuck');
-  wxLogger.info(`收到了请求 : ${JSON.stringify(req.body)}`)
-  res.json({
-    message: `收到啦~~~干嘛呢`,
-    request: JSON.stringify(req.body)
+  wxLogger.info(`服务器回调 GET: ${JSON.stringify(req.query)}`)
+
+  let echostr = req.query.echostr;
+  let signature = req.query.signature;
+  let timestamp = req.query.timestamp;
+  let nonce = req.query.nonce;
+
+  let oriArray = [];
+  oriArray.push(nonce);
+  oriArray.push(timestamp);
+  oriArray.push(token);
+
+  let original = oriArray.sort().join('');
+  let combineStr = sha1(original);
+
+  if (signature === combineStr) {
+    res.send(echostr);
+    wxLogger.info(`验证成功`)
+  } else {
+    wxLogger.info(`验证失败`)
+
+    res.render('wx_vaild', {
+      content: `验证失败:${JSON.stringify(req.query)}`
+    })
+  }
+})
+
+router.get('/deviceid', async (req, res, next) => {
+  try {
+    let res_access_token = await axios({
+      method: 'get',
+      url: 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx4943f6480a79a436&secret=13a6a97f36ad0badbbfcf5473875cb43'
+    })
+    console.log(res_access_token.data);
+
+    if (res_access_token.data.access_token) {
+      let access_token = res_access_token.data.access_token
+
+      let res_deviceid = await axios({
+        method: 'get',
+        url: decodeURI,
+        params: {
+          access_token: access_token,
+          product_id: productID
+        }
+      })
+      console.log(res_deviceid.data);
+
+      let message = JSON.stringify(res_deviceid.data)
+      res.render('get_device_id', {
+        title: `获取deviceid`,
+        result: JSON.stringify(api_token) + '  ' + message
+    })
+    }
+  } catch (err) {
+    res.render('get_device_id', {
+      title: '获取deviceid失败',
+      result: JSON.stringify(err)
+    })
+  }
+  console.log('end');
+})
+
+// 读取log页面
+router.get('/readLog', (req, res, next) => {
+  console.log(logPath);
+  console.log(`收到了请求 readLog: ${JSON.stringify(req.query)}`)
+  fs.readFile(logPath, {
+    encoding: 'utf8'
+  }, (err, data) => {
+    if (err) {
+      // console.log(`读取错误 ${JSON.stringify(err)}`)
+      res.send(JSON.stringify(err))
+    } else {
+      console.log(`读取: ${data.toString()}`)
+      readFileToArr(logPath, (arr) => {
+        res.render('wx_log', {
+          content: data.toString(),
+          lines: arr
+        })
+      })
+    }
   })
 })
 
